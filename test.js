ee1e4e1b3a143df16355dfe4ca9f527cd968ebf8
Clyde Smets <clyde.smets@gmail.com>
2023-12-19 11:16:06 +1000

    add unit tests

--- test.js
+++
@@ -1,9 +0,0 @@
-const CSSCrypt = require ('./csscrypt')
-
-const o = {
-    key: '3924834902384'
-}
-const e = CSSCrypt.encrypt("Hello!! I'm a robot. Yahoo! wee poop.s", o)
-console.log(e)
-const d = CSSCrypt.decrypt(e, o)
-console.log(d)
